72a7c3d0977ddb3d0ed9ab739a8d8f290622ce72
barbosaoli <gustavobarbosa7162@gmail.com>
2022-05-07 21:50:36 -0300

    updates

--- back-end/tests/factories/recommendationFactory.ts
+++ back-end/tests/factories/recommendationFactory.ts
@@ -1,9 +1,20 @@
 import { CreateRecommendationData } from "../../src/services/recommendationsService";
 import { faker } from "@faker-js/faker";
+import { prisma } from "../../src/database";
 
 export function recommendationFactory(): CreateRecommendationData {
   return {
     name: `${faker.music.genre()} ${faker.internet.password()}`,
     youtubeLink: "https://www.youtube.com/watch?v=iuvnaext7EU",
   };
+}
+
+export async function insertRecommendation() {
+  const data = recommendationFactory();
+
+  const dbRecommendation = await prisma.recommendation.create({
+    data,
+  });
+
+  return dbRecommendation;
 }
